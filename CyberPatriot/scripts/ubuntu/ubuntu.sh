0e8901a507c75d4d9035d01fc84701daa70c5b95
Bailey K <baileykasin@gmail.com>
2018-02-02 11:06:08 -0800

    Now installs Ubuntu Desktop.

--- CyberPatriot/scripts/ubuntu/ubuntu.sh
+++ CyberPatriot/scripts/ubuntu/ubuntu.sh
@@ -3,10 +3,11 @@
 sudo apt update
 sudo apt -y upgrade
 
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
 sudo apt -y install mysql-server
 
 sudo apt -y install mysql-client apache2 php7.0
 
-sudo apt -y install gdm
+sudo apt -y install sddm
+sudo apt -y install kubuntu-desktop
