ea6b825c571eaaefb3a8ff0dfc122be255c92b30
Dynomite567 <dalbecbr@gmail.com>
2018-09-21 09:48:47 -0700

    Updated VMWare tools to open-vm-tools

--- CyberPatriot/scripts/ubuntu/vmware-tools.sh
+++ CyberPatriot/scripts/ubuntu/vmware-tools.sh
@@ -11,22 +11,6 @@ apt update
 
 apt-get -y install perl make linux-headers-$(uname -r) xserver-xorg
 
-mkdir /mnt/vmware
-mount -o loop,ro ~/linux.iso /mnt/vmware
+apt-get -y install open-vm-tools open-vm-tools-desktop
 
-mkdir /tmp/vmware
-tar zxf /mnt/vmware/VMwareTools-*.tar.gz -C /tmp/vmware
-/tmp/vmware/vmware-tools-distrib/vmware-install.pl --default --force-install
-rm -r /tmp/vmware
-
-umount /mnt/vmware
-rm -r /mnt/vmware
-rm -f ~/linux.iso
-
-tee -a /etc/vmware-tools/locations <<EOF
-remove_answer ENABLE_VGAUTH
-answer ENABLE_VGAUTH no
-remove_answer ENABLE_VMBLOCK
-answer ENABLE_VMBLOCK no
-EOF
-/usr/bin/vmware-config-tools.pl --default --skip-stop-start
+exit 0
